cb68414e92d593ec02edfe98b56df2d36b2dafdb
Tanaphat Vivattanakijcharoen <tanaphatviva@gmail.com>
2023-05-27 14:30:07 +0700

    edit guitar style

--- src/EditGuitar.js
+++ src/EditGuitar.js
@@ -67,47 +67,51 @@ function Edit({ initialData}) {
     //window.location.reload();
   };
 
-  return (
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <div>
-        <h1>HERE YOU CAN UPLOAD YOUR GUITAR</h1>
-        <form>
-          <input type="file" id="imageInput" accept="image/jpeg, image/png" onChange={updateImage} required />
-          <label htmlFor="imageInput"></label>
-          <p>Guitar Name:</p>
-          <input type="text" id="textIn" value={name} onChange={(e) => setName(e.target.value)} required/>
-          <Filter handleChange={HandleFilterChange} />
-          <button type="button" onClick={UploadGuitar}>Apply Changes</button>
-        </form>
-
-        {successPopup && (
-          <div className="overlay">
-            <div className="popup success-popup">
-              <p>Upload successful!</p>
-              <button onClick={handlePopupClose}>Close</button>
+    return (
+      <div style={{ backgroundColor: '#ffffff', padding: '30px', paddingRight: '50px', paddingLeft: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center', boxShadow: '0px 0px 10px 2px rgba(0, 0, 0, 0.1)' }}>
+      <h1 style={{ textAlign: 'center', marginBottom: '30px' }}>Edit Your Guitar</h1>
+  
+      <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+        <div >
+          <form style={{ marginBottom: '20px' }}>
+            <label htmlFor="imageInput">Select an Image:</label>
+            <input type="file" id="imageInput" accept="image/jpeg, image/png" onChange={updateImage} required />
+            <p></p>
+            <label htmlFor="textIn">Guitar Name:</label>
+            <input type="text" id="textIn" value={name} onChange={(e) => setName(e.target.value)} required />
+            <Filter handleChange={HandleFilterChange} />
+            <button type="button" onClick={UploadGuitar}>Upload</button>
+          </form>
+  
+          {successPopup && (
+            <div className="overlay">
+              <div className="popup success-popup">
+                <p>Upload successful!</p>
+                <button onClick={handlePopupClose}>Close</button>
+              </div>
             </div>
-          </div>
-        )}
-
-        {errorPopup && (
-          <div className="overlay">
-            <div className="popup error-popup">
-              <p>Error uploading guitar.</p>
-              <button onClick={handlePopupClose}>Close</button>
+          )}
+  
+          {errorPopup && (
+            <div className="overlay">
+              <div className="popup error-popup">
+                <p>Error uploading guitar.</p>
+                <button onClick={handlePopupClose}>Close</button>
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-
-      <div>
-        <GuitarCard
-          id='imagePreview'
-          name={name}
-          body={filterSelection.body}
-          brand={filterSelection.brand}
-          pickup={filterSelection.pickup}
-          imageUrl={imageUrl}
-        />
+          )}
+        </div>
+  
+        <div >
+          <GuitarCard
+            id='imagePreview'
+            name={name}
+            body={filterSelection.body}
+            brand={filterSelection.brand}
+            pickup={filterSelection.pickup}
+            imageUrl={imageUrl}
+          />
+        </div>
       </div>
     </div>
   );
